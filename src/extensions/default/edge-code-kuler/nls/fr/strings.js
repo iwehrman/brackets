530a5f1ddc7b2afad82b3242de2e130aa0a0c6fc
wALF Utility <walf@adobe.com>
2013-07-26 17:33:44 -0700

    Updated by ALF automation.

---
+++ src/extensions/default/edge-code-kuler/nls/fr/strings.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    MANAGE_THEMES: "Manage Themes...",
+    LOADING_THEMES: "Loading Themes...",
+    NO_THEMES_1: "No themes to show.",
+    NO_THEMES_2: "Visit the <a href='https://kuler.adobe.com'>Kuler website</a> to get started.",
+    KULER_URL: "https://kuler.adobe.com",
+    MANAGE_THEMES_URL: "https://kuler.adobe.com/my/themes",
+    MY_KULER_THEMES: "My Kuler Themes",
+    FAVORITE_KULER_THEMES: "Favorite Kuler Themes"
+});
