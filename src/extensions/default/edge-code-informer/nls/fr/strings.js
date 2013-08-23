f8035f7c714d4d233fa836ef1234465b50dd60ec
Ian Wehrman <wehrman@adobe.com>
2013-08-22 17:16:11 -0700

    initial commit of informer strings

---
+++ src/extensions/default/edge-code-informer/nls/fr/strings.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+	"INFORMER_BODY_TEXT": "Aidez-nous à améliorer Edge Code. Envoyez-nous vos commentaires.",
+	"INFORMER_LEGAL_TEXT": "En cliquant sur « Envoyer », vous acceptez qu’Adobe utilise vos commentaires anonymes pour quelque usage que ce soit.",
+	"INFORMER_SUBMIT": "Envoyer",
+	"INFORMER_HEADING": "Commentaire",
+	"INFORMER_PLACEHOLDER": "Vous souhaitez donner votre avis, signaler un bug ou suggérer une fonctionnalité ? Contactez-nous !",
+	"INFORMER_THANKS": "Merci !",
+	"INFORMER_TOOLTIP": "Dialoguez avec l’équipe",
+	"INFORMER_ERROR_POST": "Vous devez être connecté pour envoyer un commentaire."
+});
