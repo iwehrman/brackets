ad3064680e3b77ce014991824f208823a458cea1
wALF Utility <walf@adobe.com>
2013-08-26 19:01:40 -0700

    Updated by ALF automation.

--- src/nls/fr/strings-app.js
+++ src/nls/fr/strings-app.js
@@ -32,6 +32,6 @@ define({
 	"ABOUT_TEXT_LINE4": "Développé à partir de Brackets - <a class=\"clickable-link\" data-href=\"https://github.com/adobe/brackets/\">https://github.com/adobe/brackets/</a>",
 	"ABOUT_TEXT_LINE5": "Un grand merci à ceux qui ont contribué à Brackets :",
 	"ABOUT_TEXT_LINE_EDGECODE_TEAM": "<b>Equipe Edge Code:</b> Bryan Chin, Jochen Hagenstr&ouml;m, Larz, Jay London, Ingo Richter, Ryan Stewart, Juliana Suh, Peter Thiess, Ian Wehrman.",
-    "DEFAULT_EDITOR_TITLE": "Default Editor",
-    "DEFAULT_EDITOR_MESSAGE": "Make {APP_NAME} your default editor for <span class='dialog-filename'>JS</span> and <span class='dialog-filename'>CSS</span> files?<br /><br />We promise we'll only ask this once."
+	"DEFAULT_EDITOR_TITLE": "Editeur par défaut",
+	"DEFAULT_EDITOR_MESSAGE": "[7126957] !é=Make {APP_NAME} your default editor for <span class='dialog-filename'>JS</span> and <span class='dialog-filename'>CSS</span> files?<br /><br />We promise we'll only ask this once.=!"
 });
